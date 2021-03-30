0c6bc997fca60f83c15492781f79d838f1696b59
Abhishek <abhishahasane@gmail.com>
2021-03-30 14:24:57 +0530

    Initial commit

---
+++ Aniruddha Bapu Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aniruddha Bapu Quotes
+//
+//  Created by Rajendra Dingankar on 1/10/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Aniruddha Bapu Quotes/Pods-Aniruddha Bapu Quotes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Aniruddha_Bapu_Quotes : NSObject
+@end
+@implementation PodsDummy_Pods_Aniruddha_Bapu_Quotes
+@end
